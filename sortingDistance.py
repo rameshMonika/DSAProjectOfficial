6ce2df3505374dd8819be193bd23925e4ffe90b6
edrichocw <edrichocw@gmail.com>
2024-03-27 16:37:14 +0800

    Added Distance Sorting Algo

---
+++ sortingDistance.py
@@ -0,0 +1,34 @@
+
+def ascendingQuickSort(arr):
+    if len(arr) <= 1:
+        return arr
+    # pivot set to median distance
+    pivot = arr[len(arr) // 2][1]
+    # left contains all elements with distance less than pivot
+    left = [x for x in arr if x[1] < pivot]
+    middle = [x for x in arr if x[1] == pivot]
+    # right contains all elements with distance greater than pivot
+    right = [x for x in arr if x[1] > pivot]
+    # recursively sort left and right
+    return ascendingQuickSort(left) + middle + ascendingQuickSort(right)
+
+def descendingQuickSort(arr):
+    if len(arr) <= 1:
+        return arr
+    # pivot set to median distance
+    pivot = arr[len(arr) // 2][1]
+    # left contains all elements with distance less than pivot
+    left = [x for x in arr if x[1] < pivot]
+    middle = [x for x in arr if x[1] == pivot]
+    # right contains all elements with distance greater than pivot
+    right = [x for x in arr if x[1] > pivot]
+    # recursively sort left and right
+    return descendingQuickSort(right) + middle + descendingQuickSort(left)
+
+def sort_by_distance(data,order):
+    if order == 'ascending':
+        return ascendingQuickSort(data)
+    elif order == 'descending':
+        return descendingQuickSort(data)
+    else:
+        raise ValueError("Invalid order. Please enter 'ascending' or 'descending'.")
